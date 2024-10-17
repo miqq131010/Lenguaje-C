70d284f55c6669f4e1bc7f561102841beadd3629
miqq131010 <garciangel131010@gmail.com>
2024-10-17 11:33:05 -0600

    l

--- archivos/archivos.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(int argc, char** argv){
-    FILE *datos = fopen("datos.txt", "r");
-    if (!datos){
-        printf("Archivo no existe\n");
-        return 1;
-    }
-    fclose(datos);
-}
