302e748e1192639c2f1f5a1dda571f05c6ac1402
miqq131010 <garciangel131010@gmail.com>
2024-10-17 11:26:38 -0600

    tEST2

--- archivos/archivos.c
+++ archivos/archivos.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(int argc, char** argv){
-    FILE *datos = fopen("datos.data", "r");
+    FILE *datos = fopen("datos.txt", "r");
     if (!datos){
         printf("Archivo no existe\n");
         return 1;
