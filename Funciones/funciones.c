c86b44e49ae571c2fef4c970863e818fa93152dd
miqq131010 <garciangel131010@gmail.com>
2024-10-22 15:30:33 -0600

    new file:   Angel - Github.code-workspace modified:   Funciones/funciones.c

--- Funciones/funciones.c
+++ Funciones/funciones.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <time.h>
-void mostrarHora() {
+void mostrarHora() {  // función que muestra la hora actual
+
     time_t t;
     struct tm *tm_info;
     time(&t);
     tm_info = localtime(&t); 
     printf("Hora actual: %02d:%02d:%02d\n", tm_info->tm_hour, tm_info->tm_min, tm_info->tm_sec);
 }
-void mostrarFecha() {
+void mostrarFecha() {   // función que muestra la fecha actual
+
     time_t t;
     struct tm *tm_info;
     time(&t); 
